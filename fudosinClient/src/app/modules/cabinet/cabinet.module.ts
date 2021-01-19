a7288a2be52c5231db69e712e9a773612405d708
Mihail Sapunov <mihail.perfect@gmail.com>
2021-01-19 22:33:00 +0400

    made normal routes

--- fudosinClient/src/app/modules/cabinet/cabinet.module.ts
+++ fudosinClient/src/app/modules/cabinet/cabinet.module.ts
@@ -4,10 +4,10 @@ import {CabinetComponent} from './cabinet/cabinet.component';
 import {HeaderComponent} from './cabinet/header/header.component';
 import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-import {PageHeaderComponent} from './cabinet/page-header/page-header.component';
 import {RouterModule} from '@angular/router';
 import {UiElementsModule} from '../ui-elements/ui-elements.module';
 import {StudentModule} from './cabinet/student/student.module';
+import {PageHeaderComponent} from './cabinet/page-header/page-header.component';
 
 
 @NgModule({
--- fudosinClient/src/app/modules/cabinet/cabinet/page-header/page-header.component.ts
+++ fudosinClient/src/app/modules/cabinet/cabinet/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+
+  constructor() {
+    this.title = '';
+  }
 
   ngOnInit(): void {
   }
