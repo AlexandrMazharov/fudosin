019fac9bc4a3344c18880eab3d69acded0fc97a8
Alexandr Azaryonok <mcconaughey.matthew@mail.ru>
2021-01-26 01:59:25 +0400

    add reg guard

--- fudosinClient/src/app/app-routing.module.ts
+++ fudosinClient/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {LoginComponent} from './components/login/login.component';
 import {CreateUserComponent} from './components/create-user/create-user.component';
 import {CabinetLogoComponent} from './modules/cabinet/cabinet/cabinet-logo/cabinet-logo.component';
 import {ResetPasswordComponent} from './components/reset-password/reset-password.component';
+import {RegGuard} from './guards/reg.guard';
 
 export const appRoutes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'lk', component: CabinetLogoComponent},
   {path: 'reset', component: ResetPasswordComponent},
-  {path: 'reg', component: CreateUserComponent},
+  {path: 'reg', component: CreateUserComponent, canActivate: [RegGuard]},
   // {path: '**', component: CabinetComponent},
 ];
 
---
+++ fudosinClient/src/app/guards/reg.guard.ts
@@ -0,0 +1,25 @@
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {TokenStorageService} from '../service/token-storage/token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegGuard implements CanActivate, CanActivateChild {
+  constructor(private tokenStorageService: TokenStorageService, private router: Router) {
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  if (this.tokenStorageService.getPerson().roles.includes('ROLE_ADMIN')) {
+    return of(true);
+  } else {
+    alert('Only admin');
+    this.router.navigate(['/lk']);
+    return  of(false);
+  }
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  return this.canActivate(route, state);
+  }
+}
