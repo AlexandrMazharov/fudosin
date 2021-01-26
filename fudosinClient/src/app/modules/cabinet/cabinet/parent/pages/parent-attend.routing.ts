45b3f801f33ee9d9835c31485a1a3d8c4d5069a5
Mihail Sapunov <mihail.perfect@gmail.com>
2021-01-26 14:45:54 +0400

    update develop

--- fudosinClient/src/app/modules/cabinet/cabinet/parent/pages/parent-attend.routing.ts
+++ fudosinClient/src/app/modules/cabinet/cabinet/parent/pages/parent-attend.routing.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
-import {CalendarMonthComponent} from '../../UI-palette/calendar/month/calendar.month.component';
-import {CalendarYearComponent} from '../../UI-palette/calendar/year/calendar.year.component';
+import {CalendarMonthComponent} from '../../../../UI-palette/calendar/month/calendar.month.component';
+import {CalendarYearComponent} from '../../../../UI-palette/calendar/year/calendar.year.component';
 
 export const parentAttendRoutes: Routes = [
   {path: 'year/:year_id', component: CalendarYearComponent},
--- fudosinClient/src/app/modules/cabinet/cabinet/student/pages/student-timetable.routing.ts
+++ fudosinClient/src/app/modules/cabinet/cabinet/student/pages/student-timetable.routing.ts
@@ -1,6 +1,11 @@
 import {Routes} from '@angular/router';
+import {CalendarYearComponent} from '../../../../UI-palette/calendar/year/calendar.year.component';
+import {CalendarMonthComponent} from '../../../../UI-palette/calendar/month/calendar.month.component';
+import {CalendarDayComponent} from '../../../../UI-palette/calendar/day/calendar.day.component';
 
 export const studentTimetableRoutes: Routes = [
-  // {path: ':page_id', component: },
-  // {path: '**', component: StudentTimetableComponent},
+  {path: ':year_id', component: CalendarYearComponent},
+  {path: ':year_id/:month_id', component: CalendarMonthComponent},
+  {path: ':year_id/:month_id/:day_id', component: CalendarDayComponent},
+  {path: '**', component: CalendarYearComponent},
 ];
