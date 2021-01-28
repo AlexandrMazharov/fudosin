e25e6c196cc44e41efb0007daa72dbda58aa44b4
Александр Mazharov <a.mazharov@gmail.com>
2021-01-28 16:28:41 +0400

    add real lesson and group add api

--- fudosinServer/src/main/java/com/example/demo/repos/VisitRepository.java
+++ fudosinServer/src/main/java/com/example/demo/repos/VisitRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repos;
 
+import com.example.demo.models.Student;
+import com.example.demo.models.TrainingGroup;
 import com.example.demo.models.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
+
 }
