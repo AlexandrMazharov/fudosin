ea9bdcc81f9915e6fdede6ba6d1c534227507f04
Alexandr Azaryonok <mcconaughey.matthew@mail.ru>
2021-02-01 13:33:28 +0400

    ad styles

--- fudosinClient/src/app/modules/account-management/account-management/account-management.component.ts
+++ fudosinClient/src/app/modules/account-management/account-management/account-management.component.ts
@@ -10,14 +10,63 @@ import {Person} from '../../../models/person.model';
 export class AccountManagementComponent implements OnInit {
 
   public persons = new Array<Person>();
-  private person: Person | undefined;
+  public chunks = [] as Person[][];
+  public currentChunk = 0;
+  private person: Person |  undefined;
+  public length: number | undefined;
 
   constructor(private accountService: AccountManagementService) {
   }
 
+  currentPageRender(): string {
+    let firstPos: string = this.currentChunk  + '1';
+    let lastPos: string = this.currentChunk + 1 + '0';
+    if (this.currentChunk === 0) {
+      firstPos = '1';
+    }
+    if (this.currentChunk + 1 === this.chunks.length) {
+      lastPos = this.persons.length + '';
+    }
+    return firstPos + ' - ' + lastPos;
+  }
+
+  increase(): void {
+    if (this.currentChunk >= this.chunks.length - 1) {
+      return;
+    } else {
+      this.currentChunk++;
+    }
+  }
+
+  decrease(): void {
+    if (this.currentChunk <= 0) {
+      return;
+    } else {
+      this.currentChunk--;
+    }
+  }
+
+  splitArr(arr: Person[]): any {
+    const newArr = [] as any[][];
+    newArr.push([]);
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 10 === 0 && i !== 0) {
+        newArr.push([]);
+        newArr[newArr.length - 1].push(arr[i]);
+      } else {
+        newArr[newArr.length - 1].push(arr[i]);
+      }
+    }
+    return newArr;
+  }
+
   getPersons(): void {
     this.accountService.getPersons()
-      .subscribe(persons => this.persons = persons);
+      .subscribe(persons => {
+        this.persons = persons;
+        this.length = this.persons.length;
+        this.chunks = this.splitArr(persons);
+      });
   }
 
   ngOnInit(): void {
