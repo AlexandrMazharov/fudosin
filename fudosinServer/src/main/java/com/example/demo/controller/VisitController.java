6cc6efda4890e74cf3dfe4f772ea5b20ec33312b
Александр Mazharov <a.mazharov@gmail.com>
2021-02-02 18:20:20 +0400

    add VisitController

--- fudosinServer/src/main/java/com/example/demo/controller/VisitController.java
+++ fudosinServer/src/main/java/com/example/demo/controller/VisitController.java
@@ -1,17 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ItemNotFoundException;
 import com.example.demo.models.Student;
 import com.example.demo.models.Visit;
 import com.example.demo.repos.StudentRepository;
 import com.example.demo.repos.VisitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
+
 @RestController
 @RequestMapping("/visit")
 @CrossOrigin(origins = {"http://localhost:4200"})
@@ -19,10 +18,35 @@ public class VisitController {
 
     @Autowired
     VisitRepository visitRepository;
+    @Autowired
+    StudentRepository studentRepository;
 
     @GetMapping
     public ResponseEntity<List<Visit>> getAllVisits() {
         return ResponseEntity.ok(visitRepository.findAll());
     }
 
+    @GetMapping("/stud")
+    public ResponseEntity<?> getVisitsByData(
+            @RequestParam Long id,
+            @RequestParam Long dateStart,
+            @RequestParam Long dateFinish
+    ) {
+        try {
+            Student student = studentRepository.findById(id).orElseThrow(() -> new ItemNotFoundException(id));
+
+            Calendar start = Calendar.getInstance();
+            start.setTimeInMillis(dateStart);
+            System.out.println(start.getTime());
+            Calendar finish = Calendar.getInstance();
+            finish.setTimeInMillis(dateFinish);
+
+            List<Visit> visits = visitRepository.findAllByStudentAndLessonTimeStartBetween(student, start.getTime(), finish.getTime());
+            return ResponseEntity.ok(visits);
+        } catch (ItemNotFoundException exception) {
+            exception.printStackTrace();
+            return ResponseEntity.ok("student not found");
+        }
+
+    }
 }
